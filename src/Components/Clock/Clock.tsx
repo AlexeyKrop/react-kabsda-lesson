9e81ba46d404d0e4981b580c1bd9268128671ac9
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-16 08:54:29 +0300

    add AnalogClockView

--- src/Components/Clock/Clock.tsx
+++ src/Components/Clock/Clock.tsx
@@ -1,9 +1,14 @@
 import React, {useEffect, useState} from "react";
 import s from './Clock.module.css'
-
-export const Clock = React.memo(() => {
+type ClockPropsType = {
+  mode: 'digital' | 'analog'
+}
+type ClockViewPropsType = {
+  date: Date
+}
+export const addZeroInClock = (num: number) => num < 10 ? '0' + num : num
+export const Clock = React.memo((props: ClockPropsType) => {
   const [date, setDate] = useState<Date>(new Date())
-  const addZeroInClock = (num: number) => num < 10 ? '0' + num : num
   useEffect(() => {
     let intervalId = setInterval(() => {
       setDate(new Date())
@@ -13,11 +18,21 @@ export const Clock = React.memo(() => {
     }
   }, [])
   return (
-      <div id={s.clock}>
-        <p className={s.date}>{`${date.getDate()} - ${addZeroInClock(date.getMonth() + 1)} - ${date.getFullYear()}`}</p>
-        <span className={s.time}>{addZeroInClock(date.getHours())}:</span>
-        <span className={s.time}>{addZeroInClock(date.getMinutes())}:</span>
-        <span className={s.time}>{addZeroInClock(date.getSeconds())}</span>
-      </div>
+     <DigitalClockView date={date}/>
   )
 })
+export const DigitalClockView: React.FC<ClockViewPropsType> = ({date}) => {
+  return(
+  <div id={s.clock}>
+    <p className={s.date}>{`${date.getDate()} - ${addZeroInClock(date.getMonth() + 1)} - ${date.getFullYear()}`}</p>
+    <span className={s.time}>{addZeroInClock(date.getHours())}:</span>
+    <span className={s.time}>{addZeroInClock(date.getMinutes())}:</span>
+    <span className={s.time}>{addZeroInClock(date.getSeconds())}</span>
+  </div>
+  )
+}
+export const AnalogClockView: React.FC<ClockViewPropsType> = ({date}) => {
+  return(
+    <span>{date}</span>
+  )
+}
